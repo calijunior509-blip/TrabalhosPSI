c4cb0e544c12b49faf5b0358d5a4d0ab620b7aee
calijunior509-blip <calijunior509@gmail.com>
2026-01-16 17:45:13 +0000

    adicionei o gerador de senha

---
+++ trabalho letra istraNHA.py
@@ -0,0 +1,35 @@
+print("---criador de palavras passes---")
+print("olá seja bem vindo ao criador de palavras passes")
+print("é de simples uso")
+print("1-tem 10 slots de palavras")
+print("pense em 10 palavras que nunca se esqueceria")
+print("(como o nome de seu animal de estimação , pai , rua , país , etc)")
+print("e o programa criará uma palavra passe pra si automaticamente")
+
+
+nome1=input("qual a primeira palavra: ")
+nome2=input("qual a segunda palavra: ")
+nome3=input("qual a terceira palavra: ")
+nome4=input("qual a quarta palavra: ")
+nome5=input("qual o quinta palavra: ")
+nome6=input("qual o sexta palavra: ")
+nome7=input("qual o setima palavra: ")
+nome8=input("qual o oitava palavra: ")
+nome9=input("qual o nona palavra: ")
+nome10=input("qual o decima palavra: ")
+
+
+print(" a sua palavra passe é "+nome1[0:2]+nome2[0:2]+nome3[0:2]+nome4[0:1]+nome5[0:2]+nome6[1:4]+nome7[0:2]+nome8[0:2]+nome9[0:2]+nome10[0:2])
+
+print("caso não esteja satisfeito podemos fazer mais uma para si")
+
+decisao=str(input("quer uma nova palavra passe?(Y/N)"))
+if decisao=="Y" or decisao=="y":
+    print("Ok , para a nova senha por favor insira os seguintes dados")
+    nome_1=str(input("qual o seu nome: "))
+    nome_2=str(input("qual a sua morada: "))
+    nome_3=str(input("qual a data que expira o seu cartão de credito: "))
+    nome_4=str(input("qual o seu numero favorito: "))
+    nome_5=str(input("qual o seu nome: "))
+
+
